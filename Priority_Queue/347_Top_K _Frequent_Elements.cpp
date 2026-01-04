b3a29b321e49ef0a980abc846361b2f5e3be1753
Rocky0412 <rockytapader4g@gmail.com>
2026-01-04 22:38:56 +0530

    Priority_Queue

---
+++ Priority_Queue/347_Top_K _Frequent_Elements.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+       unordered_map<int,int>mp;
+       for(auto ele : nums){
+        mp[ele]++;
+       }
+       priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+       for(auto [key,value] : mp){
+        pq.push({value,key});
+        if(pq.size()>k){
+            pq.pop();
+        }
+       }
+       vector<int>ans;
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
