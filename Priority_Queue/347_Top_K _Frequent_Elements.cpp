87648770df2dc643864bad394cdc809f6c9fd9de
Rocky0412 <rockytapader4g@gmail.com>
2026-01-07 00:07:24 +0530

    Leetcode 373

--- Priority_Queue/347_Top_K _Frequent_Elements.cpp
+++ Priority_Queue/347_Top_K _Frequent_Elements.cpp
@@ -23,3 +23,43 @@ public:
         return ans;
     }
 };
+
+//373. Find K Pairs with Smallest Sums
+
+class Solution {
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+       vector<vector<int>>ans;
+       priority_queue<pair<int,vector<int>>>pq;
+       for(int i=0;i<nums1.size();i++)
+       {
+            for(int j=0;j<nums2.size();j++)
+            {
+                int sum=nums1[i]+nums2[j];
+                vector<int>ops;
+                ops.push_back(nums1[i]);
+                ops.push_back(nums2[j]);
+                if(pq.size()<k)
+                {
+                    pq.push(make_pair(sum,ops));
+                }
+                else if(sum<pq.top().first)
+                {
+                    pq.pop();
+                     pq.push(make_pair(sum,ops));
+                }
+                else
+                {
+                    break;
+                }
+            }
+       }
+       while(!pq.empty())
+       {
+          ans.push_back(pq.top().second);
+          pq.pop();
+       }
+       return ans;
+        
+    }
+};
